59bffd71c57030047dd6587f643034f9d2c3b73f
Angelo Traverso <atraverso13@gmail.com>
2023-05-09 18:06:32 +0200

    You can now click "sign in" to navigate to dashboard and feel bottom NAV

--- app/src/main/java/com/example/opsc_part2/MainActivity.kt
+++ app/src/main/java/com/example/opsc_part2/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.opsc_part2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnGoToDash = findViewById<Button>(R.id.btnSignIn)
+
+
+        btnGoToDash.setOnClickListener{
+
+            intent = Intent(this, Dashboard::class.java)
+            startActivity(intent)
+
+        }
 
     }
 }
