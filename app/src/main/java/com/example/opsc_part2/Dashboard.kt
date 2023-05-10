00e03ccd9aaea769bc91c95190ec29ff2eb4d2a4
Angelo Traverso <atraverso13@gmail.com>
2023-05-10 14:56:37 +0200

    1) Sign in & Sign up Basic UI's are complete 2) Bottom Nav basic design complete

--- app/src/main/java/com/example/opsc_part2/Dashboard.kt
+++ app/src/main/java/com/example/opsc_part2/Dashboard.kt
@@ -1,11 +1,49 @@
 package com.example.opsc_part2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.opsc_part2.Dashboard
+import com.example.opsc_part2.Statistics
+import com.example.opsc_part2.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.lang.Exception
 
 class Dashboard : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        /*bottomNav.background = null  //Removing the background shadow
+        bottomNav.menu.getItem(2).isEnabled = false*/
+
+       /* loadFragment(Statistics())*/
+
+        bottomNav = findViewById(R.id.bottomNavView) as BottomNavigationView
+
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.Stats -> {
+                    loadFragment(Statistics())
+                    true
+                }R.id.Stats -> {
+                startActivity(Intent(this, Dashboard::class.java))
+                true
+            }
+                else -> {false}
+            }
+        }
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.rlParent,fragment)
+        transaction.commit()
     }
 }
