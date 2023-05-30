346c2b74d4c02b6958574a8797e268a186876584
JPolakow <jonathan@polakow.co.za>
2023-05-30 22:36:50 +0200

    added hardcoded login

--- app/src/main/java/com/example/opsc_part2/MainActivity.kt
+++ app/src/main/java/com/example/opsc_part2/MainActivity.kt
@@ -3,35 +3,67 @@ package com.example.opsc_part2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var UsernameInput: EditText
+    private lateinit var PasswordInput: EditText
+    private lateinit var btnGoToDash: Button
+    private lateinit var signUpClick: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnGoToDash = findViewById<Button>(R.id.btnSignIn)
-        val signUpClick = findViewById<TextView>(R.id.tvSignUp)
+        btnGoToDash = findViewById(R.id.btnSignIn)
+        signUpClick = findViewById(R.id.tvSignUp)
 
+        UsernameInput = findViewById(R.id.etUsername)
+        PasswordInput = findViewById(R.id.etPassword)
 
         btnGoToDash.setOnClickListener{
-
-            intent = Intent(this, Dashboard::class.java)
-            startActivity(intent)
-
+            UserLogin()
         }
 
         signUpClick.setOnClickListener{
-
             val fragmentManager = supportFragmentManager
             val transaction = fragmentManager.beginTransaction()
             transaction.add(R.id.relContainer, SignUp())
             transaction.commit()
-
         }
+    }
 
+    fun UserLogin()
+    {
+        if (UsernameInput.text.toString() == "user1" && PasswordInput.text.toString() == "password1")
+        {
+            intent = Intent(this, Dashboard::class.java)
+            startActivity(intent)
+        }
+        else
+        {
+            intent = Intent(this, Dashboard::class.java)
+           // startActivity(intent)
+
+            val errToast = Toast.makeText(applicationContext,"Incorrect username or password",Toast.LENGTH_LONG)
+            errToast.setGravity(Gravity.BOTTOM,0,25)
+            errToast.show()
+        }
     }
 }
+
+/*
+    to do
+        login
+            error message
+            valadate with db?
+
+
+
+
+ */
