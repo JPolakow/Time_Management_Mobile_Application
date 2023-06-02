643a4eecec9f95db7c468a8bf930ec17a47d9921
Angelo Traverso <atraverso13@gmail.com>
2023-06-02 18:09:38 +0200

    Small Updates here and there

--- app/src/main/java/com/example/opsc_part2/Dashboard.kt
+++ app/src/main/java/com/example/opsc_part2/Dashboard.kt
@@ -2,9 +2,11 @@ package com.example.opsc_part2
 
 import Classes.ActivityObject
 import android.content.Intent
+import android.graphics.Matrix
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
