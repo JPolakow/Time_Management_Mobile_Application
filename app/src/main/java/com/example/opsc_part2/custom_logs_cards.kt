5bc292e269c08bbe6ec8022e45594b2f79dbc7ae
Angelo Traverso <atraverso13@gmail.com>
2023-06-04 19:49:45 +0200

    I fixed Jonathon's BS

--- app/src/main/java/com/example/opsc_part2/custom_logs_cards.kt
+++ app/src/main/java/com/example/opsc_part2/custom_logs_cards.kt
@@ -2,12 +2,18 @@ package com.example.opsc_part2
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.os.Bundle
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.example.opsc_part2.databinding.FragmentSignUpBinding
+import org.w3c.dom.Text
 
 class custom_logs_cards @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -16,6 +22,8 @@ class custom_logs_cards @JvmOverloads constructor(
     init {
         LayoutInflater.from(context).inflate(R.layout.custom_logs_cards, this, true)
 
+        // Perform any initialization or customization here
+        // You can access and modify the views within the custom component layout
     }
 
     //============================================================================
