1734877716c4053a2cac584eb2c5d0214c427203
JPolakow <jonathan@polakow.co.za>
2023-06-05 14:04:39 +0200

    worked on catagory, worked on dash, got angelos timer code, and more

--- app/src/main/java/com/example/opsc_part2/Dashboard.kt
+++ app/src/main/java/com/example/opsc_part2/Dashboard.kt
@@ -5,6 +5,7 @@ import TimerManager
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
+import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
@@ -14,6 +15,7 @@ import android.view.View
 import android.view.animation.AccelerateInterpolator
 import android.widget.*
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.app.ActivityCompat
@@ -21,6 +23,8 @@ import androidx.core.app.ActivityOptionsCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlin.math.min
 
 class Dashboard : AppCompatActivity(), QuickActionPopup.DashboardFragmentListener {
@@ -28,6 +32,10 @@ class Dashboard : AppCompatActivity(), QuickActionPopup.DashboardFragmentListene
     //ui vars
     private lateinit var actionButt: FloatingActionButton
     private lateinit var bottomNav: BottomNavigationView
+    private lateinit var tvCategory: TextView
+    private lateinit var linView: LinearLayout
+
+    private var SelectedCatagory: String = ""
 
     //============================================================================
     @SuppressLint("UseCompatLoadingForDrawables")
@@ -38,25 +46,32 @@ class Dashboard : AppCompatActivity(), QuickActionPopup.DashboardFragmentListene
 
         // ======================= Declarations ======================= //
         bottomNav = findViewById(R.id.bottomNavView)
-        actionButt = findViewById<FloatingActionButton>(R.id.btnPlus)
-
-        val linView = findViewById<LinearLayout>(R.id.linearProjectCards)
-
-        val fragment = QuickActionPopup()
-        // Var to hold fragment visibility state
-        var isFragmentVisible = false
+        actionButt = findViewById(R.id.btnPlus)
+        tvCategory = findViewById(R.id.tvCategory)
+        linView = findViewById(R.id.linearProjectCards)
 
         // Obtain a reference to the ImageView
         val imgProfileImg = findViewById<ImageView>(R.id.imgProfileImg)
+
         // Create a Bitmap from the image drawable
         // This is where you change size of image
         val maxImageSize = 140
         // ======================= End Declarations ======================= //
+
         imgProfileImg.setOnClickListener {
             val intent = Intent(this, Settings::class.java)
             startActivity(intent)
         }
 
+        tvCategory.text = "Category: None"
+        tvCategory.setOnClickListener {
+            showCategoryPickerDialog { selectedCategory ->
+                tvCategory.text = "Category: $selectedCategory"
+                LoadCustomUI()
+            }
+        }
+
+        //---------------------------SET UP BOTTOM UI-------------------------
         // Create a Bitmap from the image drawable
         val drawable = resources.getDrawable(R.drawable.temp_profilepicture) as BitmapDrawable
         val bitmap = drawable.bitmap
@@ -90,6 +105,7 @@ class Dashboard : AppCompatActivity(), QuickActionPopup.DashboardFragmentListene
         params.leftMargin = 50
         params.topMargin = 25
         imgProfileImg.layoutParams = params
+        //------------------------------------END OF BOTTOM UI---------------------------------
 
         // ----------------- MENU ----------------------------
         bottomNav.setOnItemSelectedListener { menuItem ->
@@ -127,9 +143,24 @@ class Dashboard : AppCompatActivity(), QuickActionPopup.DashboardFragmentListene
         }
         // ----------------- END OF MENU -----------------------------------
 
+        LoadCustomUI()
+
+        actionButt.setOnClickListener {
+            showPopup()
+        }
+    }
+
+    //=========================================================
+    //load custom ui elements
+    private fun LoadCustomUI() {
+        linView.removeAllViews()
+
         // ----------------- Creating a new card with custom attributes ----------------- //
         for (card in ToolBox.ActivitiesList) {
-            if (card.ActivityUserID == ToolBox.ActiveUserID) {
+            if (card.ActivityUserID == ToolBox.ActiveUserID && card.ActivityCatagory.equals(
+                    SelectedCatagory
+                )
+            ) {
                 val customCard = custom_dashboard_cards(this)
                 customCard.setActivityName(card.ActivityName)
                 customCard.setActivityStartDate(card.DateCreated)
@@ -142,23 +173,38 @@ class Dashboard : AppCompatActivity(), QuickActionPopup.DashboardFragmentListene
 
                 val play = customCard.findViewById<ImageButton>(R.id.ibPausePlay)
                 val completeActivity = customCard.findViewById<ImageButton>(R.id.ibFinsih)
+                // Retrieve the timer data from shared preferences and restore the timer state
+                val startTime = TimerManager.getStartTime(this, customCard)
+                val accumulatedTime = TimerManager.getAccumulatedTime(this, customCard)
+
+                if (startTime > 0 && accumulatedTime > 0) {
+                    // Calculate the elapsed time since the start time
+                    val elapsedTime = System.currentTimeMillis() - startTime
+
+                    // Restore the timer state
+                    TimerManager.startTimer(this, customCard, timerText)
+                    TimerManager.pauseTimer(this, customCard)
+                } else {
+                    // Timer was not running before, initialize the timer state
+                    TimerManager.stopTimer(customCard)
+                    timerText.text = "00:00:00"
+                }
 
                 play.setOnClickListener {
                     if (customCard.isTimerRunning) {
                         // Pause the timer
-                        TimerManager.pauseTimer(customCard)
+                        TimerManager.pauseTimer(this, customCard)
                         play.setImageResource(R.drawable.play_circle_48px)
                         customCard.isTimerRunning = true
                     } else {
                         // Start the timer
-                        TimerManager.startTimer(customCard, timerText)
+                        TimerManager.startTimer(this, customCard, timerText)
                         play.setImageResource(R.drawable.pause_circle_48px)
                         customCard.isTimerRunning = false
 
                     }
                     customCard.isTimerRunning = !customCard.isTimerRunning
                 }
-
 
                 completeActivity.setOnClickListener {
                     val fragment = complete_activity()
@@ -177,30 +223,51 @@ class Dashboard : AppCompatActivity(), QuickActionPopup.DashboardFragmentListene
 
                 linView.addView(customCard)
             }
-            }
+        }
         // ----------------- END OF CUSTOM CARDS -------------------
-
-
-            /*
-            * If fragment is visible, hide when button is clicked
-            * Else if fragment is not visible when button clicked, then show fragment
-            * */
-            actionButt.setOnClickListener {
-                showPopup()
-            }
-        }
-
-        //============================================================================
-        override fun onFragmentRequested(fragment: Fragment) {
-            // Replace the current fragment on the dashboard with the requested fragment
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, fragment)
-                .commit()
-        }
-
-        //============================================================================
-        private fun showPopup() {
-            val fragment = QuickActionPopup()
-            fragment.show(supportFragmentManager, "QuickActionPopup")
-        }
     }
+
+    //============================================================================
+    override fun onFragmentRequested(fragment: Fragment) {
+        // Replace the current fragment on the dashboard with the requested fragment
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
+    }
+
+    //============================================================================
+    private fun showPopup() {
+        val fragment = QuickActionPopup()
+        fragment.show(supportFragmentManager, "QuickActionPopup")
+    }
+
+    //============================================================================
+    //catagory picker
+    private fun showCategoryPickerDialog(callback: (String) -> Unit) {
+
+        val catagoryNames = mutableListOf<String>()
+
+        for (item in ToolBox.CatagoryList) {
+            val secondIndexEntry = item.CatagoryName
+            catagoryNames.add(secondIndexEntry)
+        }
+
+        var displaySelected = "Catagory: ";
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Pick a catagory")
+            .setItems(catagoryNames.toTypedArray()) { dialog: DialogInterface, which: Int ->
+                val selectedCatagory = which
+
+                SelectedCatagory = catagoryNames[selectedCatagory]
+                displaySelected += SelectedCatagory
+                tvCategory.setText(displaySelected)
+                callback(SelectedCatagory)
+
+                dialog.dismiss()
+            }.setCancelable(false)
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+}
