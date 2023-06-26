cbcc3b06b5cd254d27d7cf786649d38414aaf751
Angelo Traverso <atraverso13@gmail.com>
2023-06-26 19:19:18 +0200

    Added firebase integration - can read/write

--- app/src/main/java/com/example/opsc_part2/MainActivity.kt
+++ app/src/main/java/com/example/opsc_part2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.opsc_part2
 import Classes.ActiveUserClass
 import Classes.PasswordHandler
 import Classes.ToolBox
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,6 +15,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityOptionsCompat
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,6 +29,7 @@ class MainActivity : AppCompatActivity() {
     //============================================================================
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         try {
             setContentView(R.layout.activity_main)
 
@@ -49,7 +53,44 @@ class MainActivity : AppCompatActivity() {
             ex.printStackTrace()
         }
     }
+    //============================================================================
+    private fun writeToDB()
+    {
+        val db = Firebase.firestore
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
 
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
+    }
+
+    //============================================================================
+    private fun readFromDB()
+    {
+        val db = Firebase.firestore
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+    }
     //============================================================================
     private fun userLogin() {
         try {
