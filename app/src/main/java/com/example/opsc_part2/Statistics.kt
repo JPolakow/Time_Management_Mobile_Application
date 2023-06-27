a43a63ee5b882d90e58a4456f1dda47b321f0e3e
Angelo Traverso <atraverso13@gmail.com>
2023-06-27 18:17:01 +0200

    Implemented pie chart. Functionally works without using retrieved data from firebase

--- app/src/main/java/com/example/opsc_part2/Statistics.kt
+++ app/src/main/java/com/example/opsc_part2/Statistics.kt
@@ -3,15 +3,22 @@ package com.example.opsc_part2
 import Classes.ToolBox
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
 
 class Statistics : Fragment(R.layout.fragment_statistics) {
 
     private lateinit var linView: LinearLayout
+    private lateinit var pieChart: PieChart
 
     //============================================================================
     override fun onCreateView(
@@ -19,13 +26,57 @@ class Statistics : Fragment(R.layout.fragment_statistics) {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_statistics, container, false)
         try {
-            linView = view.findViewById(R.id.linearProjectCards)
+           linView = view.findViewById(R.id.linearProjectCards)
+
+            pieChart = view.findViewById(R.id.chart)
+            initPieChart()
+            loadPieChartData()
+
             populate()
         } catch (ex: java.lang.Exception) {
             Log.w("log", ex.toString())
             ex.printStackTrace()
         }
         return view
+    }
+    //============================================================================
+    // Method to initialise pie chart
+    // All pie chart properties will go here
+    private fun initPieChart() {
+        pieChart.setUsePercentValues(true)
+        // setting pie chart description visibility
+        pieChart.description.isEnabled = false
+        // setting legend visibility
+        pieChart.legend.isEnabled = true
+        pieChart.setDrawEntryLabels(false)
+        // setting rotation of pie chart = true
+        pieChart.isRotationEnabled = true
+        // Enabling pie chart hole
+        pieChart.isDrawHoleEnabled = true
+        // Setting hole size of pie chart
+        pieChart.holeRadius = 0f
+        pieChart.setHoleColor(android.R.color.transparent)
+        // setting drag friction for dragging pie chart
+        pieChart.dragDecelerationFrictionCoef = 0.95f
+    }
+
+    //============================================================================
+    // This is where we will load our own data
+    private fun loadPieChartData() {
+        val entries = mutableListOf<PieEntry>()
+        entries.add(PieEntry(30f, "John"))
+        entries.add(PieEntry(40f, "Jake"))
+        entries.add(PieEntry(50f, "Peter"))
+
+        val dataSet = PieDataSet(entries, "Sample Pie Chart")
+        dataSet.colors = listOf(Color.CYAN, Color.BLUE, Color.MAGENTA)
+        dataSet.valueTextColor = Color.BLACK
+        dataSet.valueTextColor = Color.BLACK
+        dataSet.valueTextSize = 12f
+
+        val data = PieData(dataSet)
+        pieChart.data = data
+        pieChart.invalidate()
     }
 
     //============================================================================
