dd7f8b7b390d5d2bb275311fbf9a97a3542fa330
JPolakow <jonathan@polakow.co.za>
2023-07-02 18:40:41 +0200

    trophy system implemented

--- app/src/main/java/com/example/opsc_part2/AchievementsFragment.kt
+++ app/src/main/java/com/example/opsc_part2/AchievementsFragment.kt
@@ -1,19 +1,58 @@
 package com.example.opsc_part2
 
+import Classes.ActivityObject
+import Classes.ToolBox
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-// Here ":" symbol is indicate that LoginFragment
-// is child class of Fragment Class
 class AchievementsFragment : Fragment() {
+
+    private lateinit var trophyimg: ImageView
+    private lateinit var trophyname: TextView
+    private lateinit var trophyamount: TextView
+    private var Amount: Int = 0
+
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(
-            R.layout.fragment_achievements_settings, container, false
-        )
+        val view = inflater.inflate(R.layout.fragment_achievements_settings, container, false)
+
+        trophyname = view.findViewById(R.id.tvTrophyName)
+        trophyimg = view.findViewById(R.id.imgTrophy)
+        trophyamount = view.findViewById(R.id.tvTrophyAmount)
+
+        trophyname.text = "Goals Achieved"
+        trophyimg.setImageResource(R.drawable.trophygrey)
+        trophyamount.text = "Amount: $Amount"
+
+
+        val filteredActivity = ToolBox.ActivitiesList.filter { activity ->
+            activity.ActivityUserID == ToolBox.ActiveUserID
+        }
+        val filteredWE = ToolBox.WorkEntriesList.filter { WE ->
+            WE.WEUserID == ToolBox.ActiveUserID
+        }
+
+        for (activity in filteredActivity) {
+            val totalDuration =
+                filteredWE.filter { it.WEActivityName == activity.ActivityName }
+                    .sumBy { it.WEDuration.toInt() }
+
+            if (activity.ActivityMaxGoal <= totalDuration) {
+                Amount++
+                trophyimg.setImageResource(R.drawable.trophyincolor)
+            }
+        }
+
+        trophyamount.text = "Amount: $Amount"
+
+        return view
     }
 }
