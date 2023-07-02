df17948932733353c3359ce2bcfd461d7a30ab2c
JPolakow <jonathan@polakow.co.za>
2023-07-02 18:42:41 +0200

    made angelo happy

--- app/src/main/java/com/example/opsc_part2/AchievementsFragment.kt
+++ app/src/main/java/com/example/opsc_part2/AchievementsFragment.kt
@@ -18,21 +18,24 @@ class AchievementsFragment : Fragment() {
     private lateinit var trophyamount: TextView
     private var Amount: Int = 0
 
+    //============================================================================
     @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_achievements_settings, container, false)
 
+        //Front end vars
         trophyname = view.findViewById(R.id.tvTrophyName)
         trophyimg = view.findViewById(R.id.imgTrophy)
         trophyamount = view.findViewById(R.id.tvTrophyAmount)
 
-        trophyname.text = "Goals Achieved"
+        //Default values
+        trophyname.text = "Max goals achieved"
         trophyimg.setImageResource(R.drawable.trophygrey)
         trophyamount.text = "Amount: $Amount"
 
-
+        //Get lists for user
         val filteredActivity = ToolBox.ActivitiesList.filter { activity ->
             activity.ActivityUserID == ToolBox.ActiveUserID
         }
@@ -40,6 +43,7 @@ class AchievementsFragment : Fragment() {
             WE.WEUserID == ToolBox.ActiveUserID
         }
 
+        //Find activities with duration over max goal
         for (activity in filteredActivity) {
             val totalDuration =
                 filteredWE.filter { it.WEActivityName == activity.ActivityName }
